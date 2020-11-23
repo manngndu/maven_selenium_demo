3b087c180cbc10e30f9437cd00bd96bf7e4923ee
manngndu <manngndu@gmail.com>
2020-11-23 14:17:45 +0530

    added the files

---
+++ src/test/java/com/practice/tests/DemoTest.java
@@ -0,0 +1,43 @@
+package com.practice.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\manpreesingh\\Downloads\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+	@Test
+	public void sumTest() {
+		System.out.println("TESTING SUM");
+		int a = 20;
+		int b = 30;
+		int sum = a + b;
+		Assert.assertEquals(sum, 50);
+	}
+
+	@Test
+	public void subTest() {
+		System.out.println("TESTING SUBTRACTION");
+		int a = 50;
+		int b = 30;
+		int sub = a - b;
+		Assert.assertEquals(sub, 20);
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+}
